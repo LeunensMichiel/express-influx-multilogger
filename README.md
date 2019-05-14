cdefb2a3c62405eda607ee06cc70acf05dc58534
Michiel Leunens <michiel.leunens@gmail.com>
2019-05-14 15:12:01 +0200

    updated readme.md and abstracted code for better visibility and scaleability

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 An Express middleware for better monitoring of your Node.js apps.
 Parse important req, res and header objects to Influx and Grafana. Get an easier insight of your API without any costs.
 
+**Note:** this is in _active_ development, and could contain bugs. Please make an issue if you find some.
+
 ## Getting started
 
 ### Installation
@@ -12,10 +14,10 @@ Parse important req, res and header objects to Influx and Grafana. Get an easier
    npm install express-influx-multilogger
    ```
 2. Require
-    ```
-    const multilogger = require('express-influx-multilogger');
-    ```
-3. (optional if you want to write to influx) Initialize database in app.js
+   ```
+   const multilogger = require('express-influx-multilogger');
+   ```
+3. (Required if you want to write to influx) Initialize database in your app.js
    ```
    multilogger.init({
     database: {
@@ -25,25 +27,28 @@ Parse important req, res and header objects to Influx and Grafana. Get an easier
          },
     interval: 10000
    });
-    ```
-4.  Add multilogger log function right before router
-    ```
-    app.use(multilogger.log({ development: false, extended: false }));
-    ```
-5.  Add multierror function before catching 404 and after your router
-    ```    
-    app.use(multilogger.error());
-    ```
-6.  In case of using geolocation, make sure your server can make use of req.connection.remoteAddress
+   ```
+4. Add multilogger log function right before your router of choice
+   ```
+   app.use(multilogger.log({ development: false, extended: false }));
+   ```
+5. Add multierror function before catching 404 and after your router
+   ```
+   app.use(multilogger.error());
+   ```
+6. In case of using geolocation, make sure your server can make use of req.connection.remoteAddress
+
 ### Example
 
 ```
 const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-const multilogger = require("./logger/MultiloggerWare"); // Add multilogger
+const multilogger = require('express-influx-multilogger'); // Add this to imports
 
 const indexRouter = require("./routes/index");
+
+//  Add this to write data to influx (not required)
 multilogger.init({
     database: {
         server: "127.0.0.1",
@@ -59,17 +64,25 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+// Add this before your router of choice
 app.use(multilogger.log({ development: false, extended: false }));
 
 app.use("/", indexRouter); // Your router
 
+// Add this before 404 if you want to capture your errors as well
 app.use(multilogger.error());
 
 module.exports = app;
 ```
 
 ### Loggable headers
-Fields
+
+The data being sent to Influx consists of ...
+
+#### Fields
+
+...logs of the amount of calls per statuscode per given interval
+
 ```
     amountOf1xx: Influx.FieldType.INTEGER,
     amountOf2xx: Influx.FieldType.INTEGER,
@@ -77,6 +90,9 @@ Fields
     amountOf4xx: Influx.FieldType.INTEGER,
     amountOf5xx: Influx.FieldType.INTEGER
 ```
+
+...logs of Response Time, CPU and memory Usage of the OS, host and ip;
+
 ```
     responseTime: Influx.FieldType.FLOAT,
     cpuUsage: Influx.FieldType.FLOAT,
@@ -85,7 +101,11 @@ Fields
     host: Influx.FieldType.STRING,
     ip: Influx.FieldType.STRING
 ```
-Tags
+
+#### Tags
+
+...logs of basic headers to Influx
+
 ```
     "statusCode",
     "statusMessage",
@@ -94,25 +114,38 @@ Tags
     "url",
     "ip",
     "country",
-    "geohash",
+    "geohash", // Used to get the geolocation of your api call from a given IP
     "client",
     "body",
     "query",
     "params",
-    "errorMessage",
+    "auth", // In case you wanna know wich user that triggered the api call
+    "errorMessage", // In case of an error: sends message and stack
     "errorStack"
 ```
+
 ### Parameters
 
 1. Extended: Logs a pretty view of req, res and headers (defaults false)
 2. Development: Logs the object that Influx will recieve
 3. Database:
-    *   server: The address of your Influx database. _(defaults: 127.0.0.1)_
-    *   name: Name of your Influx database. _(defaults: myMultilogDb)_
-    *   port: Port of your Influx database. _(defaults: 3000)_
-    *   username: Login credentials of your Influx database. _(defaults: '')_
-    *   password: Password of your Influx database. _(defaults: '')_
+   - server: The address of your Influx database. _(defaults: 127.0.0.1)_
+   - name: Name of your Influx database. _(defaults: myMultilogDb)_
+   - port: Port of your Influx database. _(defaults: 3000)_
+   - username: Login credentials of your Influx database. _(defaults: '')_
+   - password: Password of your Influx database. _(defaults: '')_
 4. Interval: Defines the rate in ms of the interval you want to write your data to your Influx database
+
+### Grafana
+
+<img src="./img/screenshot.png" width="800"><br>
+<img src="./img/screenshot2.png" width="800">
+
+If you want to use Grafana and don't want to write your dashboard, you can import
+[the dashboard JSON](./grafanaDashboard.json). You can certainly write your own dashboard, as I'm not that skilled with Grafana.
+
+By using this dashboard, make sure you install [the worldmap plugin](https://grafana.com/plugins/grafana-worldmap-panel). 
+
 
 ## License
 
@@ -120,11 +153,12 @@ Distributed under the MIT License. See `LICENSE` for more information.
 
 ## Dependencies
 
-*   [systeminformation](https://github.com/sebhildebrandt/systeminformation)
-*   [lodash](https://lodash.com/)
-*   [node-influx](https://www.npmjs.com/package/influx)
-*   [ngeohash](https://www.npmjs.com/package/ngeohash)
-*   [iplocation](https://www.npmjs.com/package/iplocation)
+- [systeminformation](https://github.com/sebhildebrandt/systeminformation)
+- [lodash](https://lodash.com/)
+- [node-influx](https://www.npmjs.com/package/influx)
+- [ngeohash](https://www.npmjs.com/package/ngeohash)
+- [iplocation](https://www.npmjs.com/package/iplocation)
 
 ## Contributors
-*   [Michiel Cuvelier](https://github.com/cuvelierm)
+
+- [Michiel Cuvelier](https://github.com/cuvelierm)
--- multilogger.js
+++ multilogger.js
@@ -1,329 +1,44 @@
-const si = require("systeminformation");
-const Influx = require("influx");
-const _ = require("lodash");
-const iplocation = require("iplocation").default;
-const geohash = require("ngeohash");
-
 let data = [];
 
 module.exports = {
-    init: ({
-               interval = 1000,
-               database: {
-                   server = "127.0.0.1",
-                   name = "myMultilogDb",
-                   password = "",
-                   port = 3000,
-                   username = ""
-               } = {}
-           }) => {
-        return init(interval, server, name, password, port, username);
-    },
-    log: ({ extended = false, development = false }) => {
-        return log(extended, development);
-    },
-    error: () => {
-        return throwMultilogError();
-    }
-};
-
-//  Initialize the middleware, start an interval to write de buffer data to a database of choice
-const init = (interval, server, name, password, port, username) => {
-    setInterval(() => {
-        InitDatabase(server, name, password, port, username).catch(err =>
-            console.error(err.stack)
-        );
-    }, interval);
-};
-
-async function writeToDatabase(influx, name) {
-    influx
-        .getDatabaseNames()
-        .then(names => {
-            if (!names.includes(name)) {
-                return influx.createDatabase(name);
-            }
-        })
-        .catch(err => {
-            console.error(`Error creating Influx database!`);
-        });
-
-    if (_.size(data) > 0) {
-        await influx
-            .writePoints([
-                {
-                    measurement: "number_of_requests",
-                    fields: {
-                        requests: data.length,
-                        amountOf1xx: _.size(
-                            _.filter(data, object => {
-                                return _.startsWith(object.statusCode, "1");
-                            })
-                        ),
-                        amountOf2xx: _.size(
-                            _.filter(data, object => {
-                                return _.startsWith(object.statusCode, "2");
-                            })
-                        ),
-                        amountOf3xx: _.size(
-                            _.filter(data, object => {
-                                return _.startsWith(object.statusCode, "3");
-                            })
-                        ),
-                        amountOf4xx: _.size(
-                            _.filter(data, object => {
-                                return _.startsWith(object.statusCode, "4");
-                            })
-                        ),
-                        amountOf5xx: _.size(
-                            _.filter(data, object => {
-                                return _.startsWith(object.statusCode, "5");
-                            })
-                        )
-                    },
-                    tags: {}
-                }
-            ])
-            .catch(err => {
-                console.error(`Error saving data to InfluxDB! ${err.stack}`);
-            });
-    }
-
-    if (_.size(data) > 0) {
-        for (const object of data) {
-            await influx
-                .writePoints([
-                    {
-                        measurement: "multilogger",
-                        tags: {
-                            statusCode: object.statusCode,
-                            statusMessage: object.statusMessage,
-                            method: object.method,
-                            path: object.path,
-                            url: object.url,
-                            ip: object.ip,
-                            country: object.location.country || " ",
-                            geohash: object.location.geohash || " ",
-                            client: object.clientInfo,
-                            body: object.body,
-                            query: object.query,
-                            params: object.params,
-                            errorMessage:
-                                JSON.stringify(object.errorMessage.errorMessage) || " ",
-                            errorStack: JSON.stringify(object.errorMessage.errorStack) || " "
-                        },
-                        fields: {
-                            responseTime: object.responseTime,
-                            cpuUsage: object.cpuUsage.avg,
-                            memoryUsage: object.memoryUsage.used,
-                            requests: data.length,
-                            host: object.hostname,
-                            ip: object.ip
-                        }
-                    }
-                ])
-                .catch(err => {
-                    console.error(`Error saving data to InfluxDB! ${err.stack}`);
-                });
-        }
-    }
-
-    data = [];
-}
-
-//  Writes the buffer to the database
-const InitDatabase = async (server, name, password, port, username) => {
-    const influx = new Influx.InfluxDB({
-        host: server,
-        database: name,
-        port,
-        password,
-        username,
-        schema: [
-            {
-                measurement: "number_of_requests",
-                fields: {
-                    requests: Influx.FieldType.INTEGER,
-                    amountOf1xx: Influx.FieldType.INTEGER,
-                    amountOf2xx: Influx.FieldType.INTEGER,
-                    amountOf3xx: Influx.FieldType.INTEGER,
-                    amountOf4xx: Influx.FieldType.INTEGER,
-                    amountOf5xx: Influx.FieldType.INTEGER
-                },
-                tags: []
-            },
-            {
-                measurement: "multilogger",
-                fields: {
-                    responseTime: Influx.FieldType.FLOAT,
-                    cpuUsage: Influx.FieldType.FLOAT,
-                    memoryUsage: Influx.FieldType.FLOAT,
-                    requests: Influx.FieldType.INTEGER,
-                    host: Influx.FieldType.STRING,
-                    ip: Influx.FieldType.STRING
-                },
-                tags: [
-                    "statusCode",
-                    "statusMessage",
-                    "method",
-                    "path",
-                    "url",
-                    "ip",
-                    "country",
-                    "geohash",
-                    "client",
-                    "body",
-                    "query",
-                    "params",
-                    "errorMessage",
-                    "errorStack"
-                ]
-            }
-        ]
-    });
-
-    await writeToDatabase(influx, name);
-};
-
-// Creates a log object
-const log = (extended, development) => {
-    return async (req, res, next) => {
-        const startHrTime = process.hrtime();
-        const realBody = _.isEmpty(req.body) ? " " : JSON.stringify(req.body);
-        const cpuUsage = await getCpuInfo();
-        const memoryUsage = await getMemInfo();
-
-        res.on("finish", async () => {
-            const elapsedHrTime = process.hrtime(startHrTime);
-            const elapsedTimeInMs = elapsedHrTime[0] * 1000 + elapsedHrTime[1] / 1e6;
-            if (extended) {
-                getBasic(req, res);
-                getParameters(req, realBody);
-                getAuth(req);
-                getPerformance(cpuUsage, memoryUsage);
-            }
-            const location = await iplocation(req.connection.remoteAddress)
-                .then(result => {
-                    result.geohash = geohash.encode(result.latitude, result.longitude);
-                    console.log(result);
-                    return result;
-                })
-                .catch(err => {});
-            const object = {
-                method: req.method,
-                statusCode: res.statusCode,
-                statusMessage: res.statusMessage,
-                date: new Date().toUTCString(),
-                responseTime: elapsedTimeInMs,
-                contentType: req.header("Content-Type") || " ",
-                hostname: req.hostname,
-                url: req.url,
-                path:
-                    res.statusCode !== 404 && req.route && req.route.path
-                        ? req.route.path
-                        : "No Path",
-                body: req.method === "POST" ? realBody : " ",
-                params: _.isEmpty(req.params) ? " " : JSON.stringify(req.params),
-                query: _.isEmpty(req.query) ? " " : JSON.stringify(req.query),
-                cookies: _.isEmpty(req.cookies) ? " " : JSON.stringify(req.cookies),
-                auth: req.header("Authorization") || " ",
-                ip: req.connection.remoteAddress,
-                location: location,
-                clientInfo: req.header("User-Agent") || " ",
-                memoryUsage: memoryUsage,
-                cpuUsage: cpuUsage,
-                errorMessage: res.locals.multiError || " "
-            };
-            if (development) {
-                console.log(object);
-            }
-            data.push(object);
-        });
-        next();
-    };
-};
-
-// FANCY LOGS
-const getBasic = (req, res) => {
-    console.log("\n=====- Multilogger v0.1 -=====");
-    console.log("--- Basic ---\n");
-    console.info(
-        `${req.method} ––– ${res.statusCode} –––  ${
-            res.statusMessage
-            } at ${new Date().toLocaleString()}`
+  init: ({
+    interval = 1000,
+    database: {
+      type = "influx",
+      server = "127.0.0.1",
+      name = "myMultilogDb",
+      password = "",
+      port = 3000,
+      username = ""
+    } = {}
+  }) => {
+    return databaseInitializer.initializer(
+      server,
+      name,
+      password,
+      port,
+      username,
+      type,
+      interval
     );
-    console.info(`Response-time: ${res.getHeader("X-Response-Time")}`);
-    console.info(
-        `Content Type: ${req.header("Content-Type") || "No content type given"}`
-    );
-    console.info(`Hostname: ${req.hostname}`);
-    console.info(
-        `Path & URL: ${(req.route && req.route.path) || "No Path"} ––– ${req.url}`
-    );
-};
-const getParameters = (req, realBody) => {
-    console.log("\n--- Parameters ---\n");
-    if (realBody) {
-        console.info(`Request body: ${realBody}`);
-    } else {
-        console.info(`Request body: Body was empty`);
-    }
-    if (req.params && Object.keys(req.params).length !== 0) {
-        console.info(`Parameters: ${JSON.stringify(req.params)}`);
-    } else {
-        console.info("Parameters: No parameters given");
-    }
-    if (req.query && Object.keys(req.query).length !== 0) {
-        console.info(`Query: ${JSON.stringify(req.query)}`);
-    } else {
-        console.info("Query: No query given ❓");
-    }
-    console.info(
-        `Cookies & Storage: ${JSON.stringify(req.cookies) || "No tasty cookies 🍪"}`
-    );
-};
-const getAuth = req => {
-    console.log("\n--- Authorization ---\n");
-    console.info(
-        `Authorization: ${req.header("Authorization") ||
-        "No authorization given ⛔"}`
-    );
-    console.info(
-        `Client: ${req.ip || "No IP found"} ––– ${req.header("User-Agent")}`
-    );
-};
-const getPerformance = (cpuInfo, memoryInfo) => {
-    console.log("\n--- Performance ---\n");
-
-    console.info(`Memory Usage: ${JSON.stringify(memoryInfo)}`);
-    console.info(`CPU Usage: ${JSON.stringify(cpuInfo)}`);
+  },
+  log: ({ extended = true, development = false }) => {
+    return logger(extended, development);
+  },
+  error: () => {
+    return multiError();
+  },
+  pushToData: object => {
+    return data.push(object);
+  },
+  emptyData: () => {
+    return (data = []);
+  },
+  getData: () => {
+    return data;
+  }
 };
 
-//  GET CPU INFO
-const getCpuInfo = () => {
-    return si.cpuCurrentspeed();
-};
-
-//  GET MEMORY INFO
-const getMemInfo = async () => {
-    const mem = await si.mem();
-    return {
-        free: mem.free,
-        used: mem.used,
-        total: mem.total
-    };
-};
-
-//  THROW A CUSTOM ERROR AND ADD IT TO THE MIDDLEWARE
-const throwMultilogError = () => {
-    return (err, req, res, next) => {
-        if (!err) {
-            return next();
-        }
-        res.locals.multiError = {
-            errorMessage: err.message,
-            errorStack: err.stack
-        };
-        next();
-    };
-};
+const logger = require("./lib/Logger");
+const multiError = require("./lib/MultiError");
+const databaseInitializer = require("./lib/DatabaseInitializer");
